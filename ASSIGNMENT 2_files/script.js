f2ce7f6434392068bded8cb4bad42844ecfca843
ranaaliunantenne <ranaaliunantenne@gmail.com>
2026-09-22 15:03:49 +1000

    added files for assignment 2

---
+++ ASSIGNMENT 2_files/script.js
@@ -0,0 +1,95 @@
+// get video element and store it in "video" variable
+
+// 
+const playPauseBtn = document.querySelector("#play-pause-btn");
+
+
+const playPauseImg = document.querySelector("#play-pause-img");
+const progressBar = document.querySelector("#progress-bar-fill");
+video.removeAttribute("controls");
+
+// Volume control
+const volume = document.querySelector("#volume");
+volume.addEventListener("input", function () {
+  video.volume = volume.value;
+});
+
+// playPauseBtn.addEventListener("click", togglePlayPause);
+video.addEventListener("timeupdate", updateProgressBar);
+
+function togglePlayPause() {
+
+  const video = document.getElementById("custom-video-player");
+    const audio = document.getElementById("mode-audio");
+    const playPauseImg = document.getElementById("play-pause-img");
+
+    video.addEventListener("timeupdate", updateProgressBar);
+  if (video.paused) {
+        video.play();
+        audio.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+  } else {
+    video.pause();
+    audio.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  }
+}
+function updateProgressBar() {
+  const video =document.getElementById("custom-video-player");
+  const progressBar = document.getElementById("progress-bar-fill");
+
+  if (video.duration) {
+    const value = (video.currentTime / video.duration) * 100;
+    progressBar.style.width = value + "%";
+  }
+}
+// Add other functionalities here
+function showPage(page) {
+
+  const video =document.querySelector("#custom-video-player");
+  const videoSource = document.querySelector("#video-source");
+  const audio = document.querySelector("#mode-audio");
+  const audioSource = document.querySelector("#audio-source");
+   
+  document.body.classList.remove(
+  "nature-theme",
+  "study-theme",
+  "creative-theme",
+  "home-theme"
+  )
+
+  if (page ==="nature") {
+    document.body.classList.add("nature-theme");
+     videoSource.src = "ASSIGNMENT 2_files/nature.mp4";
+    audioSource.src = "ASSIGNMENT 2_files/nature music.mp3";
+   
+  
+  }
+
+  else if (page === "study") {
+    document.body.classList.add("study-theme");
+      videoSource.src = "ASSIGNMENT 2_files/study.mp4";
+    audioSource.src = "ASSIGNMENT 2_files/study music.mp3";
+  
+  }
+
+  else if (page === "creative"){
+    document.body.classList.add("creative-theme");
+    videoSource.src = "ASSIGNMENT 2_files/creative.mp4";
+    audioSource.src = "ASSIGNMENT 2_files/creative music.mp3";
+    
+
+  }
+
+  else if (page ==="home") {
+    document.body.classList.add("home-theme");
+    videoSource.src = "ASSIGNMENT 2_files/midnight.mp4";
+    audioSource.src = "ASSIGNMENT 2_files/midnight music.mp3";
+    
+      
+  }
+  video.load();
+    audio.load();
+    video.currentTime = 0;
+  document.getElementById("progress-bar-fill").style.width = "0%";
+}
